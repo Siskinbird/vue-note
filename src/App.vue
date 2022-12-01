d1567d42b05d24ae4cab0feb7ba78fcd03c394ac
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-01 16:28:31 +0300

    init: Note app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <div id="app">
+            <h1>We are start</h1>
+            <img alt="Vue logo" src="./assets/logo.png">
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
