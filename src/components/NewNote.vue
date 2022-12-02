8765fc505f1ef3c7624f5ccae5a967ddcc271923
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-02 07:24:30 +0300

    add: newNotes component

---
+++ src/components/NewNote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="new-note">
+    <label for="">Title</label>
+    <input v-model="note.title" type="text" value="">
+    <label for="">Description</label>
+    <textarea v-model="note.description"></textarea>
+    <button class="btn btnPrimary" @click="addNote">Post</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      require: true
+    }
+  },
+  methods: {
+    addNote() {
+      this.$emit('addNote', this.note)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .new-note {
+    text-align: center;
+  }
+</style>
