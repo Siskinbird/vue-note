93212573ece2715333c40ea396d4a3465d476ea4
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-02 07:25:17 +0300

    add: App settings

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,38 @@
       <section>
         <div class="container">
           <div id="app">
-            <h1>We are start</h1>
-            <img alt="Vue logo" src="./assets/logo.png">
+                  <!--message-->
+            <Message v-if="message" :message="message"/>
+                  <!--new note-->
+            <NewNote :note="note"  @addNote="addNote" />
+
+                  <!--note title-->
+            <div class="note-title" style="margin-top: 20px">
+              <h1>{{ title }}</h1>
+
+                 <!--search-->
+              <Search :search="search"
+                      @search="search = $event"
+                      placeholder="Find your note"
+                      value=""
+              />
+
+              <div class="icons">
+                <svg :class="{active: grid}" @click="grid = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
+                  <rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect>
+                  <rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect>
+                </svg>
+                <svg :class="{active: !grid}" @click="grid = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line>
+                  <line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line>
+                  <line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line>
+                </svg>
+              </div>
+            </div>
+                  <!--note list-->
+            <Notes :notes="notesFilter"  :grid="grid" @remove="removeNote" />
           </div>
         </div>
       </section>
@@ -16,9 +46,87 @@
 <script>
 
 
-export default {
+import Message from "@/components/Message";
+import NewNote from "@/components/NewNote";
+import Notes from "@/components/Notes";
+import Search from "@/components/Search";
 
+export default {
+  components: {Notes, NewNote, Message, Search},
+  data() {
+    return {
+      title: 'Notes App',
+      message: null,
+      grid: true,
+      search: '',
+      note: {
+        title: '',
+        description: ''
+      },
+      notes: [
+        {
+          title: 'First note',
+          description: 'Desc for first note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Second note',
+          description: 'Desc for Second note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+          title: 'Third note',
+          description: 'Desc for Third note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+      ]
+    }
+  },
+  computed: {
+    notesFilter () {
+      let array = this.notes,
+          search = this.search
+      if (!search) {
+        return array
+      } else {
+        search.trim().toLowerCase()
+        array = array.filter(function (item) {
+          if (item.title.toLowerCase().indexOf(search) !== -1) {
+            return item
+          }
+        })
+        return array;
+      }
+    }
+  },
+  methods: {
+    reset(){
+      this.note.title = '';
+      this.note.description = ''
+    },
+    addNote () {
+      let {title, description} = this.note;
+
+      if (title === '' || description === '') {
+        this.message = 'You note is empty';
+        return true
+      } else {
+        this.notes.push({
+          title,
+          description,
+          date: new Date(Date.now()).toLocaleString()
+        })
+        this.reset();
+        return this.message = false
+
+      }
+    },
+    removeNote(i) {
+      this.notes.splice(i, 1)
+    },
+  }
 }
+
 
 </script>
 
