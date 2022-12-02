b66f8dc68f2fca536a1a75d797e0cab8caa91389
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-02 07:24:59 +0300

    add: Message component

---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message">
+    <p>Message - {{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.message {
+  text-align: center;
+  color: #b53f3f;
+}
+
+</style>
