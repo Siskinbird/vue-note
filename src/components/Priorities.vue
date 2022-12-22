400493ca7693a13539eea2d398a3950e5c41a418
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-22 09:58:08 +0300

    add: favico

--- src/components/Priorities.vue
+++ src/components/Priorities.vue
@@ -11,7 +11,6 @@ export default {
    data () {
      return {
        priorities: [
-
          {id: 0, alias: 'base', title: 'Simple'},
          {id: 1, alias: 'medium', title: 'Hard'},
          {id: 2, alias: 'hard', title: 'Extra hard'}
